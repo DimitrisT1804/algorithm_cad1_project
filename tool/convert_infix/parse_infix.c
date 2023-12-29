255b98fd7d7592ee4d8815acd84c2a0c30ba3086
Dimitris Tsalapatas <dhmhtrhss393@gmail.com>
2023-12-29 21:35:16 +0200

    working version of infix to postfix

--- tool/convert_infix/parse_infix.c
+++ tool/convert_infix/parse_infix.c
@@ -5,13 +5,17 @@ int getPriority(char operator)
 {
     if(operator == '!')
     {
-        return 3;
+        return 4;
     }
     else if(operator == '*')
     {
-        return 2;
+        return 3;
     }
     else if(operator == '+')
+    {
+        return 2;
+    }
+    else if(operator == '^')
     {
         return 1;
     }
@@ -25,7 +29,6 @@ char* parse_infix(char *infix)
 {
     int i;
     stack *operatorStack;
-    stack *outStack;
     int infix_size = 0;
     char *postfix = NULL;
     int postfix_pos = 0;
@@ -35,7 +38,6 @@ char* parse_infix(char *infix)
     postfix = (char*) malloc(sizeof(char) * infix_size);
 
     operatorStack = create_stack(infix_size);
-    outStack = create_stack(infix_size);
 
     for(i = 0; i < infix_size; i++)
     {
@@ -50,16 +52,21 @@ char* parse_infix(char *infix)
             {
                 while (!isEmpty(operatorStack) && operatorStack->array[operatorStack->top] != '(')
                 {
-                    push(outStack, pop(operatorStack));
+                    postfix[postfix_pos] = pop(operatorStack);
+                    postfix_pos++ ;
                 }
-                pop(operatorStack);
+                pop(operatorStack); // discard ( //
                 
+            }
+            else if(getPriority(token) == 0)
+            {
+                postfix[postfix_pos] = token;
+                postfix_pos++ ;
             }
             else
             {
-                while(!isEmpty(operatorStack) && getPriority(operatorStack->array[operatorStack->top]) > getPriority(token))
+                while(!isEmpty(operatorStack) && getPriority(operatorStack->array[operatorStack->top]) >= getPriority(token))
                 {
-                    // push(outStack, pop(operatorStack));
                     postfix[postfix_pos] = pop(operatorStack);
                     postfix_pos++ ;
                 }
@@ -70,21 +77,11 @@ char* parse_infix(char *infix)
 
     while(!isEmpty(operatorStack))
     {
-        push(outStack, pop(operatorStack));
-        // postfix[postfix_pos] = pop(operatorStack);
-        // postfix_pos++ ;
+        postfix[postfix_pos] = pop(operatorStack);
+        postfix_pos++ ;
     }
 
-    while (!isEmpty(outStack))
-    {
-        //strcat(postfix, (char*) outStack->array[outStack->top]);
-        postfix[postfix_pos] = outStack->array[outStack->top];
-        postfix_pos++;
-        pop(outStack);
-    }
     
-
-
     postfix[postfix_pos] = '\0';
 
     return postfix;
@@ -92,7 +89,7 @@ char* parse_infix(char *infix)
 
 int main()
 {
-    char infix[] = "!(A+B)";
+    char infix[] = "!((I0 * !(S)) + (I1 * S) + (I0 * I1)) ";
 
     printf("The postfix is %s\n", parse_infix(infix));
 
