057ec6ebaf391dc336b4f6bd61d50e780b1d6cbe
dhmhtrhs393 <dhmhtrhs393@gmail.com>
2024-01-17 05:06:04 +0200

    begin of toposort functions

--- tool/CUDD_impl/toposort.c
+++ tool/CUDD_impl/toposort.c
@@ -1,3 +1,60 @@
 // Toposort algorithm source file //
 #include "toposort.h"
 
+Gatepin_pos* init_gatepin_pos(Gatepin_pos *gatepin)
+{
+    gatepin = (Gatepin_pos *) malloc(sizeof(Gatepin_pos));
+    gatepin->ghash = NULL;
+    gatepin->gdepth = NULL;
+
+    return gatepin;
+}
+
+void add_gatepin_pos(Gatepin_pos *gatepin, int ghash, int gdepth)
+{
+    gatepin->ghash = (int *) realloc(gatepin->ghash, sizeof(int) * (gatepin->size + 1));
+    gatepin->gdepth = (int *) realloc(gatepin->gdepth, sizeof(int) * (gatepin->size + 1));
+    gatepin->ghash[gatepin->size] = ghash;
+    gatepin->gdepth[gatepin->size] = gdepth;
+
+    gatepin->size++;
+}
+
+void remove_gatepin_pos(Gatepin_pos *gatepin, int ghash)
+{
+    int i, j;
+    for (i = 0; i < gatepin->size; i++)
+    {
+        if (gatepin->ghash[i] == ghash)
+        {
+            for (j = i; j < gatepin->size - 1; j++)
+            {
+                gatepin->ghash[j] = gatepin->ghash[j + 1];
+                gatepin->gdepth[j] = gatepin->gdepth[j + 1];
+            }
+            gatepin->size--;
+            break;
+        }
+    }
+}
+
+void free_gatepin_pos(Gatepin_pos *gatepin)
+{
+    free(gatepin->ghash);
+    free(gatepin->gdepth);
+    free(gatepin);
+}
+
+void toposort()
+{
+    Gatepin_pos *L = init_gatepin_pos(L);
+    Gatepin_pos *S = init_gatepin_pos(S);
+
+    while(S->size != 0)
+    {
+        add_gatepin_pos(L, S->ghash[0], S->gdepth[0]);
+        remove_gatepin_pos(S, S->ghash[0]);
+
+        
+    }
+}
--- tool/CUDD_impl/toposort.h
+++ tool/CUDD_impl/toposort.h
@@ -2,11 +2,19 @@
 
 struct gatepin_pos
 {
-    int ghash;
-    int gdepth;
+    int *ghash;
+    int *gdepth;
+    int size;
 };
 typedef struct gatepin_pos Gatepin_pos;
 
 
 Gatepin_pos *sorted_gatepins;   // L //
 Gatepin_pos *input_nodes;       // S //
+
+Gatepin_pos *init_gatepin_pos(Gatepin_pos *gatepin);
+void add_gatepin_pos(Gatepin_pos *gatepin, int ghash, int gdepth);
+void remove_gatepin_pos(Gatepin_pos *gatepin, int ghash);
+void free_gatepin_pos(Gatepin_pos *gatepin);
+
+// End of snippet //
